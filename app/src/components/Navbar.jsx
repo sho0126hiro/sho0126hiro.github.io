fa2f118a86c2b7c1ae139b67b26f3341cdf3aa79
sho0126hiro <sho0126hiro@gmail.com>
2020-05-26 16:55:13 +0900

    navbar layout fin

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import posed from 'react-pose'
 
-export default () => (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">about</Link>
-        <Link to="/project">project</Link>
-    </div>
-)
+const itemText = "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+export default class Navbar extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { isOpened: false }
+	}
+
+	toggleIsOpened = () => {
+		this.setState({ isOpened: !this.state.isOpened })
+	}
+
+	close = () => {
+		this.setState({ isOpened: false})
+	}
+
+	render() {
+		return (
+			<div className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+				<div className="flex items-center flex-shrink-0 text-white mr-6">
+					<svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
+					<span className="font-semibold text-xl tracking-tight">sho0126hiro</span>
+				</div>
+				<div className="block lg:h-0">
+					<button className="lg:hidden flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+						onClick={this.toggleIsOpened}>
+						<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+					</button>
+				</div>
+				<div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${this.state.isOpened ? "open ease-in-out" : "close ease-in-out"}`}>
+
+					<div className="text-sm lg:flex-grow">
+						<Link to="/" className={`${itemText} ${this.state.isOpened? "open-text":"close-text" }`} onClick={this.close}>Home</Link>
+						<Link to="/about" className={`${itemText} ${this.state.isOpened? "open-text":"close-text" }`} onClick={this.close}>About</Link>
+						<Link to="/project" className={`${itemText} ${this.state.isOpened? "open-text":"close-text" }`} onCLick={this.close}>Project</Link>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
