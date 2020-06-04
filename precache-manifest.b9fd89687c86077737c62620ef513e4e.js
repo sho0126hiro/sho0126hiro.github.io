70bf37a1517ac6ec835446b022ccc2861c45aa84
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-04 07:53:19 +0000

    Deploy sho0126hiro/sho0126hiro.github.io to github.com/sho0126hiro/sho0126hiro.github.io.git:master

--- precache-manifest.b9fd89687c86077737c62620ef513e4e.js
+++ precache-manifest.b9fd89687c86077737c62620ef513e4e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d12f955ee500cd75f4259cc0965c4119",
+    "revision": "7b87ba39ed8f917f4f005a8c7b59b1bd",
     "url": "/index.html"
   },
   {
-    "revision": "c93db75df45d9c3348ed",
+    "revision": "09ccae935785b4f3c361",
     "url": "/static/css/main.01a24064.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5e36232f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c93db75df45d9c3348ed",
-    "url": "/static/js/main.5ff46199.chunk.js"
+    "revision": "09ccae935785b4f3c361",
+    "url": "/static/js/main.d5f1d7da.chunk.js"
   },
   {
     "revision": "d07f77faf7a395ab7587",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bb65f4eacb891b9fbe52f66fd21857c.js"
+  "/precache-manifest.b9fd89687c86077737c62620ef513e4e.js"
 );
 
 self.addEventListener('message', (event) => {
