9c5ebbd00837197cfb5c50123c7a2f146eb4c3aa
sho0126hiro <sho0126hiro@gmail.com>
2020-09-07 03:25:53 +0900

    add intern

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -88,10 +88,37 @@ export default () => (
 			<div className="career md:my-8" id="career">
 				<label className="text-xl pl-5 my-3 font-semibold md:text-3xl md:block">Career</label>
 				<div className="md:px-16">
-					<div className="px-6 mt-1 md:mt-2">
-						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://lecre.jp/">株式会社ルクレ
-							<span className="text-xs pl-2 m-1 text-gray-700 md:text-sm lg:text-base">2018.08 (2week)</span></a>
-						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">CreateJSを用いて動きのあるリッチなWebアプリケーションを開発しました。</p>
+					<div className="px-6 mt-1 mb-2 lg:mt-4">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://www.optim.co.jp/">株式会社オプティム
+						<span className="text-xs m-1 ml-2 text-gray-700 md:text-sm lg:text-base">2020.09 (2week)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">Ruby on Railsを用いたBtoBサービスの機能追加</p>
+						<hr />
+					</div>
+					
+					<div className="px-6 mt-1 mb-2 lg:mt-4">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://www.acroquest.co.jp/">Acroquest Technology 株式会社
+						<span className="text-xs m-1 ml-2 text-gray-700 md:text-sm lg:text-base">2020.09 (1day)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">Python + Kerasを用いた機械学習ハンズオン</p>
+						<hr />
+					</div>
+					<div className="px-6 mt-1 mb-2 lg:mt-4">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://www.team-lab.com/">チームラボ株式会社
+						<span className="text-xs m-1 ml-2 text-gray-700 md:text-sm lg:text-base">2020.08 (2week)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">BtoCサービスのアプリ向けバックエンド開発（Java+PlayFramework）</p>
+						<hr />
+					</div>
+					<div className="px-6 mt-1 mb-2 lg:mt-4">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://info.cookpad.com/">クックパッド株式会社
+						<span className="text-xs m-1 ml-2 text-gray-700 md:text-sm lg:text-base">2020.04 (1day)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">Rubyを用いたCookpad模擬アプリケーションのパフォーマンスチューニングを行いました。</p>
+						<hr />
+					</div>
+					
+					
+					<div className="px-6 mt-1 lg:mt-4">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://www.askul.co.jp/kaisya/">アスクル株式会社
+						<span className="text-xs m-1  ml-2 text-gray-700 md:text-sm lg:text-base">2019.08 (1week)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">個人向け通販サイト<a href="https://lohaco.jp" className="text-blue-500">Lohaco</a>のサービス改善のための提案および開発を行いました。</p>
 						<hr />
 					</div>
 					<div className="px-6 mt-1 lg:mt-4">
@@ -100,16 +127,10 @@ export default () => (
 						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">シンプルな静的サイトのフロントエンド開発やReactJSを用いたTodoアプリ、およびPHP(Laravel)を用いてWebサービスを開発しました。</p>
 						<hr />
 					</div>
-					<div className="px-6 mt-1 lg:mt-4">
-						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://www.askul.co.jp/kaisya/">アスクル株式会社
-				<span className="text-xs m-1  ml-2 text-gray-700 md:text-sm lg:text-base">2019.08 (1week)</span></a>
-						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">個人向け通販サイト<a href="https://lohaco.jp" className="text-blue-500">Lohaco</a>のサービス改善のための提案および開発を行いました。</p>
-						<hr />
-					</div>
-					<div className="px-6 mt-1 mb-2 lg:mt-4">
-						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://info.cookpad.com/">クックパッド株式会社
-				<span className="text-xs m-1 ml-2 text-gray-700 md:text-sm lg:text-base">2020.04 (1day)</span></a>
-						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">Rubyを用いたCookpad模擬アプリケーションのパフォーマンスチューニングを行いました。</p>
+					<div className="px-6 mt-1 md:mt-2">
+						<a className="px-2 text-sm text-blue-500 mg:text-base lg:text-lg" href="https://lecre.jp/">株式会社ルクレ
+							<span className="text-xs pl-2 m-1 text-gray-700 md:text-sm lg:text-base">2018.08 (2week)</span></a>
+						<p className="text-xs mx-4 mt-1 mb-2 md:text-sm lg:text-base lg:mb-4 lg:my-2 md:mx-8">CreateJSを用いて動きのあるリッチなWebアプリケーションを開発しました。</p>
 						<hr />
 					</div>
 				</div>
